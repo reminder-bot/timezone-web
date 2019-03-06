84a54efec4e52e123b7539b0a36deb12db1efd05
jude-lafitteIII <judewrs@gmail.com>
2019-03-06 08:18:39 +0000

    getting username and id from oauth

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ extern crate urlencoding;
 
 use actix_web::{server, http, App, HttpResponse, Result, Query, client, AsyncResponder, Error, HttpMessage, HttpRequest};
 use actix_web::middleware::session::{RequestSession, SessionStorage, CookieSessionBackend};
-// use askama::Template;
+use askama::Template;
 use sha2::{Sha512Trunc224 as Sha, Digest};
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::env;
@@ -22,7 +22,18 @@ use base64::{encode as b64encode};
 use urlencoding::encode as uencode;
 use futures::Future;
 
+
+const DISCORD_BASE: &str = "https://discordapp.com/api/v6";
+
 struct AppState;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    logged_in: bool,
+    user: String,
+    login_redir: String,
+}
 
 #[derive(Debug, Serialize)]
 struct TokenRequest {
@@ -63,12 +74,56 @@ struct OAuthAccess {
     scope: String,
 }
 
-
-fn create_auth_url<'a>(redirect: &'a str, sid: &'a str) -> String {
-    format!("https://discordapp.com/api/oauth2/authorize?response_type=code&client_id={}&scope=identify%20guilds&redirect_uri={}&state={}", env::var("CLIENT_ID").unwrap(), uencode(redirect), uencode(sid))
+#[derive(Deserialize)]
+struct DiscordUser {
+    id: String,
+    username: String,
+    discriminator: String,
 }
 
-fn index(req: &HttpRequest<AppState>) -> Result<HttpResponse> {
+
+fn create_auth_url<'a>(redirect: &'a str, sid: &'a str) -> String {
+    format!("{}/oauth2/authorize?response_type=code&client_id={}&scope=identify%20guilds&redirect_uri={}&state={}", DISCORD_BASE, env::var("CLIENT_ID").unwrap(), uencode(redirect), uencode(sid))
+}
+
+fn index(req: HttpRequest<AppState>) -> Box<Future<Item = HttpResponse, Error = Error>> {
+    if let Some(discord_token) = req.session().get::<String>("access_token").unwrap() {
+        client::ClientRequest::get(&format!("{}/users/@me", DISCORD_BASE))
+            .header("Authorization", format!("Bearer {}", discord_token).as_str())
+            .finish().unwrap()
+            .send()
+            .map_err(|m| {
+                println!("{:?}", m);
+                Error::from(m)
+            })
+            .and_then(
+                move |resp| {
+                    resp.json::<DiscordUser>()
+                        .map_err(|m| {
+                            println!("{:?}", m);
+                            Error::from(m)
+                        })
+                        .and_then(move |body| {
+                            let i = IndexTemplate { logged_in: true, user: body.username.clone(), login_redir: req.url_for_static("login").unwrap().to_string() };
+
+                            Ok(HttpResponse::Ok()
+                                .content_type("text/html")
+                                .body(i.render().unwrap()))
+                        })
+                })
+            .responder()
+    }
+    else {
+        let i = IndexTemplate { logged_in: false, user: String::from(""), login_redir: req.url_for_static("login").unwrap().to_string() };
+        Box::new(req.body().map_err(Error::from).map(move |_f| {
+            HttpResponse::Ok()
+                .content_type("text/html")
+                .body(i.render().unwrap())
+            }))
+    }
+}
+
+fn login(req: &HttpRequest<AppState>) -> Result<HttpResponse> {
     if let Some(_discord_token) = req.session().get::<String>("access_token")? {
         Ok(HttpResponse::build(http::StatusCode::OK)
             .content_type("text/html")
@@ -108,7 +163,7 @@ fn oauth((query, req): (Query<OAuthQuery>, HttpRequest<AppState>)) -> Box<Future
             let code = &query.code;
             let c = TokenRequest::new(code.clone(), req.url_for_static("oauth").unwrap().to_string());
 
-            client::ClientRequest::post("https://discordapp.com/api/v6/oauth2/token")
+            client::ClientRequest::post(format!("{}/oauth2/token", DISCORD_BASE).as_str())
                 .header("Content-Type", "application/x-www-form-urlencoded")
                 .form(c).unwrap()
                 .send()
@@ -160,7 +215,11 @@ fn main() {
                 )
             .resource("/", |r| {
                 r.name("index");
-                r.method(http::Method::GET).f(index)
+                r.method(http::Method::GET).with(index)
+            })
+            .resource("/login", |r| {
+                r.name("login");
+                r.method(http::Method::GET).f(login)
             })
             .resource("/oauth", |r| {
                 r.name("oauth");
